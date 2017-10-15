eda330cbafb178d5639d019e6e6fe48bef48df60
Luis Custodio <luis.custodio@gmail.com>
2017-10-16 00:08:40 +0200

    Revert "feat(pages): add pages and enhance index (#2)" (#3)
    This reverts commit 489661db3d4782e1b14df60bf805597ad3682d95.

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
 const Curriculum = () => (
   <div>
     <h1>Curriculum Vitae</h1>
     <p>Math!</p>
+    <Link to="/">Go back to the homepage</Link>
   </div>
 );
 
