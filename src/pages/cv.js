489661db3d4782e1b14df60bf805597ad3682d95
Luis Custodio <luis.custodio@gmail.com>
2017-10-16 00:07:40 +0200

    feat(pages): add pages and enhance index (#2)
    * add skeleton for pages
    * enhance layout index

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Link from 'gatsby-link';
 
 const Curriculum = () => (
   <div>
     <h1>Curriculum Vitae</h1>
     <p>Math!</p>
-    <Link to="/">Go back to the homepage</Link>
   </div>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import styles from './index.module.css';
+
+import 'typeface-dancing-script';
 
 const interests = [
-  'Statistics', 'Linguistics', 'R', 'Applied Math'
+  'Population Genetics',
+  'Statistics',
+  'Apply Math',
+  'R',
+  'Systems Biology',
+  'Politics',
+  'Languages',
+  'Caenorhabditis elegans',
+  'Arabidopsis thaliana'
 ];
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi, I'm Helena</h1>
-    <p>PhD Student, Université Grenoble Alpes</p>
-    <h2>My main point of interest are:</h2>
-    <ul>
-      {interests.map(interest => <li key={interest}>{interest}</li>)}
-    </ul>
-    <hr />
-    <img src={'https://avatars1.githubusercontent.com/u/11591145?v=4&s=460'} />
-  </div>
+  <main className={styles.container}>
+    {/*<p>PhD Student, Université Grenoble Alpes</p>*/}
+    <div className={styles.image}>
+      <img src={'https://avatars1.githubusercontent.com/u/11591145?v=4&s=460'}/>
+    </div>
+    <div className={styles.interests}>
+      <h2 className={styles.interestsTitle} style={{fontFamily: 'Dancing Script'}}>
+        My main point of interest are:</h2>
+      <ul>
+        {interests.map(interest => <li key={interest}>{interest}</li>)}
+      </ul>
+
+    </div>
+  </main>
 );
 
 export default IndexPage;
