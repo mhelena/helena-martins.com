3a84feb4639faf6378c19dd356bb5690ae2b3c70
mhelena <ola.helenamartins@gmail.com>
2017-11-14 15:36:50 +0100

    docs(education): update thesis title

--- src/pages/education.js
+++ src/pages/education.js
@@ -4,13 +4,9 @@ const Education = () => (
   <div>
     <h1 id="education">Education</h1>
     <ul>
-      <li><p><strong>Ph.D. Student, TIMC-IMAG, Grenoble, France. (2015 – current)</strong> Thesis: <em>Ancestral
-        Population Differentiation Statistics to Identify Local Adaptation</em></p></li>
-      <li><p><strong>MSc Automation and Systems Engineering, Universidade Federal de Santa Catarina, Florianópolis,
-        Brazil. (2011-2014)</strong> Thesis: <em>A Petri Net model for exploring the Vulva’s Development in the worm
-        Caenorhabditis elegans</em></p></li>
-      <li><p><strong>Bachelor in Mathematics, Universidade Federal de Santa Catarina, Florianópolis, Brazil
-        (2006-2010)</strong> Thesis: <em>Sequences and Series of Functions</em></p></li>
+      <li><p><strong>Ph.D. Student, TIMC-IMAG, Grenoble, France. (2015 – current)</strong> Thesis: <em>Statistics Methods to Identify Local Adaptation in Continuous Population</em></p></li>
+      <li><p><strong>MSc Automation and Systems Engineering, Universidade Federal de Santa Catarina, Florianópolis, Brazil. (2011-2014)</strong> Thesis: <em>A Petri Net model for exploring the Vulva’s Development in the worm Caenorhabditis elegans</em></p></li>
+      <li><p><strong>Bachelor in Mathematics, Universidade Federal de Santa Catarina, Florianópolis, Brazil (2006-2010)</strong> Thesis: <em>Sequences and Series of Functions</em></p></li>
     </ul>
   </div>
 );
