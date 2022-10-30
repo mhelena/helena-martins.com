e2930e52d4305d9216a68bdf142beaac78198d9e
Luis Custodio <luis.custodio@gmail.com>
2022-10-30 19:28:32 +0000

    update education structure (#22)

--- src/pages/education.js
+++ src/pages/education.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Education = () => (
   <div>
     <h1 id="education">Education</h1>
-    <ul>
-      <li><p><strong>PhD in Biostatistics, Université Grenoble Alpes, Grenoble, France. (2015 – 2018)</strong> Thesis: <em>Statistics Methods to Identify Local Adaptation in Continuous Populations</em></p></li>
-      <li><p><strong>MSc Automation and Systems Engineering, Universidade Federal de Santa Catarina, Florianópolis, Brazil. (2011-2014)</strong> Thesis: <em>A Petri Net model for exploring the Vulva’s Development in the worm Caenorhabditis elegans</em></p></li>
-      <li><p><strong>Bachelor in Mathematics, Universidade Federal de Santa Catarina, Florianópolis, Brazil (2006-2010)</strong> Thesis: <em>Sequences and Series of Functions</em></p></li>
-    </ul>
+    <h3>PhD in Biostatistics, Université Grenoble Alpes, Grenoble, France. (2015 – 2018)</h3>
+    <p>Thesis: <em>Statistics Methods to Identify Local Adaptation in Continuous Populations</em></p>
+    <h3>MSc Automation and Systems Engineering, Universidade Federal de Santa Catarina, Florianópolis, Brazil. (2011-2014)</h3>
+    <p> Thesis: <em>A Petri Net model for exploring the Vulva’s Development in the worm Caenorhabditis elegans</em></p>
+    <h3>Bachelor in Mathematics, Universidade Federal de Santa Catarina, Florianópolis, Brazil (2006-2010)</h3>
+    <p>Thesis: <em>Sequences and Series of Functions</em></p>
   </div>
 );
 
