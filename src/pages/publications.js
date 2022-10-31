8a0388bd444e01b7538ef607385e2436ad6264b2
Luis Custodio <luis.custodio@gmail.com>
2022-10-31 20:01:28 +0000

    adds recent articles

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -1,26 +1,141 @@
-import React from 'react';
+import React from "react";
 
 const Publications = () => (
   <div>
     <h1 id="publications">Publications</h1>
-    <h3 id="articles-published-in-scientific-journals"><strong>Articles published in scientific journals:</strong></h3>
-    <p><strong>Martins H</strong>, Caye K, Luu K, Blum MGB Identifying outlier loci in admixed and in continuous
-      populations using ancestral population differentiation statistics. <em>Molecular Ecology</em> 25, 1365-294X.</p>
-    <p>Caye K, Deist TM, <strong>Martins H</strong>, Michel O, François O (2016). TESS3: Fast inference of spatial
-      population structure and genome scans for selection. <em>Molecular Ecology Resources</em> 16, 540-548.</p>
-    <p>François O, <strong>Martins H</strong>, Caye K, Schoville SD (2016). Controlling false discoveries in genome
-      scans for selection. <em>Molecular Ecology</em> 25, 454-469.</p>
-    <p><strong>Martins H</strong>, Cury JER, Queiroz MH (2014). A Petri Net model for exploring the Vulva’s Development
-      in the worm C. <em>elegans</em>. <em>Anais do XX Congresso Brasileiro de Automática</em>.</p>
-    <p><strong>Martins H</strong>, Martins JL (2008). Closed Formula for the sum of infinite series involving terms of
-      the Fibonacci sequence. <em>Revista da Olimpíada Regional de Matemática de Santa Catarina</em>. 6, 71.</p>
-    <h3 id="published-books"><strong>Published books:</strong></h3>
-    <p><strong>Martins H</strong>, Martins JL (2011). Elements of Differential and Integral Calculus. Editora UFOP.</p>
-    <h3 id="technical-production"><strong>Technical production:</strong></h3>
-    <p>Pinho JLR, <strong>Martins H</strong> (2009). Journal of Regional Mathematics Olympiad of Santa Catarina, 6 -
-      Editorial</p>
-    <p>Pinho JLR,<strong>Martins H</strong> (2008). Journal of Regional Mathematics Olympiad of Santa Catarina, 5 -
-      Editorial</p>
+
+    <h3 id="journals">Articles published in scientific journals</h3>
+
+    <p>
+      Campbell, C., Leu, C., Feng, Y. A., Wolking, S., Moreau, C., Ellis, C.,
+      Ganesan, S., <strong>Martins, H. </strong>, Oliver, K., Boothman, I.,
+      Benson, K., Molloy, A., Brody, L., Michaud, J. L., Hamdan, F. F.,
+      Minassian, B. A., & Cavalleri, G. L. (2022).{" "}
+      <strong>
+        The role of common genetic variation in presumed monogenic epilepsies.
+      </strong>{" "}
+      eBioMedicine, 81, 104098.
+      <p>
+        <a href="https://doi.org/10.1016/j.ebiom.2022.104098">
+          https://doi.org/10.1016/j.ebiom.2022.104098
+        </a>
+      </p>
+    </p>
+
+    <p>
+      Katri Silvennoinen, Kinga Gawel, Despina Tsortouktzidis, Julika Pitsch,
+      Saud Alhusaini, Karen M. J. van Loo, Richard Picardo, Zuzanna Michalak,
+      Susanna Pagni, <strong>Helena Martins</strong>, James Mills, Christopher
+      D. Whelan, Greig I. de Zubicaray, Katie L. McMahon, Wietske van der Ent,
+      Karolina J. Kirstein-Smardzewska, Ettore Tiraboschi, Jonathan M. Mudge,
+      Adam Frankish, Maria Thom, Margaret J. Wright, Paul M. Thompson, Susanne
+      Schoch, Albert J. Becker, Camila V. Esguerra & Sanjay M. Sisodiya (2022).{" "}
+      <strong>
+        SCN1A overexpression, associated with a genomic region marked by a risk
+        variant for a common epilepsy, raises seizure susceptibility.
+      </strong>{" "}
+      Acta Neuropathol 144, 107–127 (2022).
+      <p>
+        <a href="https://doi.org/10.1007/s00401-022-02429-0">
+          https://doi.org/10.1007/s00401-022-02429-0
+        </a>
+      </p>
+    </p>
+
+    <p>
+      Braatz, V., <strong>Martins, H.</strong>, Leu, C., Agrò, L., Wang, B.,
+      Calafato, S., Rayner, G., Doyle, M.G., Hengsbach, C., Bisulli, F., Weber,
+      Y.G., Gambardella, A., Delanty, N., Cavalleri, G., Foong, J., Scheffer,
+      I.E., Berkovic, S.F., Bramon, E., Balestrini, S. and Sisodiya, S.M.
+      (2021),{" "}
+      <strong>Postictal Psychosis in Epilepsy: A Clinicogenetic Study.</strong>{" "}
+      Ann Neurol, 90: 464-476.
+      <p>
+        <a href="https://doi.org/10.1002/ana.26174">
+          https://doi.org/10.1002/ana.26174
+        </a>
+      </p>
+    </p>
+
+    <p>
+      Katri Silvennoinen, Clinda Puvirajasinghe, Kirsty Hudgell, Meneka K.
+      Sidhu, <strong>Helena Martins</strong>, Wendy D. Jones, Simona Balestrini,
+      Sanjay M. Sisodiya. Genomics England Research Consortium.{" "}
+      <strong>
+        Late diagnoses of Dravet syndrome: How many individuals are we missing?
+      </strong>{" "}
+      Epilepsia Open. 2021; 6:e12525.
+      <p>
+        <a href="https://doi.org/10.1002/epi4.12525">
+          https://doi.org/10.1002/epi4.12525
+        </a>
+      </p>
+    </p>
+
+    <p>
+      Medine I. Gulcebi, Emanuele Bartolini, Omay Lee, Christos Panagiotis
+      Lisgaras, Filiz Onat, Janet Mifsud, Pasquale Striano, Annamaria Vezzani,
+      Michael S. Hildebrand, Diego Jimenez-Jimenez, Larry Junck, David
+      Lewis-Smith, Ingrid E. Scheffer, Roland D. Thijs, Sameer M. Zuberi,
+      Stephen Blenkinsop, Hayley J. Fowler, Aideen Foley,{" "}
+      <strong>Epilepsy Climate Change Consortium</strong>, Sanjay M. Sisodiya.
+      <strong>
+        Climate change and epilepsy: Insights from clinical and basic science
+        studies.
+      </strong>{" "}
+      Epilepsy & Behavior, Volume 116, 107791
+      <p>
+        <a href="https://doi.org/10.1016/j.yebeh.2021.107791">
+          https://doi.org/10.1016/j.yebeh.2021.107791
+        </a>
+      </p>
+    </p>
+
+    <p>
+      <strong>Martins H</strong>, Caye K, Luu K, Blum MGB Identifying outlier
+      loci in admixed and in continuous populations using ancestral population
+      differentiation statistics. <em>Molecular Ecology</em> 25, 1365-294X.
+    </p>
+    <p>
+      Caye K, Deist TM, <strong>Martins H</strong>, Michel O, François O (2016).
+      TESS3: Fast inference of spatial population structure and genome scans for
+      selection. <em>Molecular Ecology Resources</em> 16, 540-548.
+    </p>
+    <p>
+      François O, <strong>Martins H</strong>, Caye K, Schoville SD (2016).
+      Controlling false discoveries in genome scans for selection.{" "}
+      <em>Molecular Ecology</em> 25, 454-469.
+    </p>
+    <p>
+      <strong>Martins H</strong>, Cury JER, Queiroz MH (2014). A Petri Net model
+      for exploring the Vulva’s Development in the worm C. <em>elegans</em>.
+      <em>Anais do XX Congresso Brasileiro de Automática</em>.
+    </p>
+    <p>
+      <strong>Martins H</strong>, Martins JL (2008). Closed Formula for the sum
+      of infinite series involving terms of the Fibonacci sequence.{" "}
+      <em>Revista da Olimpíada Regional de Matemática de Santa Catarina</em>. 6,
+      71.
+    </p>
+
+    <h3 id="books">Published books</h3>
+
+    <p>
+      <strong>Martins H</strong>, Martins JL (2011). Elements of Differential
+      and Integral Calculus. Editora UFOP.
+    </p>
+
+    <h3 id="technical-production">Technical production</h3>
+
+    <p>
+      Pinho JLR, <strong>Martins H</strong> (2009). Journal of Regional
+      Mathematics Olympiad of Santa Catarina, 6 - Editorial
+    </p>
+
+    <p>
+      Pinho JLR,<strong>Martins H</strong> (2008). Journal of Regional
+      Mathematics Olympiad of Santa Catarina, 5 - Editorial
+    </p>
   </div>
 );
 
